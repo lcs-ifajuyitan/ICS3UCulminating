a237e167f4c32c747de87d6523bcfa92a9375560
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-06-01 14:40:41 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by ☆ on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
